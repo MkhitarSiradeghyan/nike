bed5e2eda4f91ab4e1d5d62f224b45895521c367
MkhitarSiradeghyan <mkhitar.siradeghyan@yandex.ru>
2025-11-03 17:17:41 +0400

    feat: base folders

---
+++ components/Example/Example.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import s from "./Example.module.css"
+
+const Example = () => {
+  return (
+    <div className={s.example}>Example</div>
+  )
+}
+
+export default Example
