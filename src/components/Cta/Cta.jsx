cf73e545b1ea96dcab6e29440458a3d50b0126a8
unknown <miqayelqeshishoxlyan@gmail.com>
2025-11-11 17:20:39 +0400

    feat: cta-form

--- src/components/Cta/Cta.jsx
+++ src/components/Cta/Cta.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
 import s from "./Cta.module.css"
+import Container from '../Container/Container'
 
 const Cta = () => {
   return (
-    <div className={s.container}>
+    <div className="cta">
+      <Container>
+        <div className={s.body}>
         <h1 className={s.tittle}>STAY UPTO DATE ABOUT <br /> OUR LATEST OFFERS</h1>
         <input className={s.input} type="text" />
         <div className={s.button}>Subscribe to Newsletter</div>
     </div>
+    </Container>
+    </div>
+    
+
   )
 }
 
