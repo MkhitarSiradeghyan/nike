3b1f60e7b4aed0f6270692b20733d5b7d86f002c
unknown <miqayelqeshishoxlyan@gmail.com>
2025-11-14 18:01:01 +0400

    feat: cta-form

---
+++ src/components/Cta/Cta.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import s from "./Cta.module.css"
+import Container from '../Container/Container'
+
+const Cta = () => {
+  return (
+    <div className="cta">
+      <Container>
+        <div className={s.body}>
+        <h1 className={s.tittle}>STAY UPTO DATE ABOUT <br /> OUR LATEST OFFERS</h1>
+        .
+        <input className={s.input} type="text" />
+        <div className={s.button}>Subscribe to Newsletter</div>
+    </div>
+    </Container>
+    </div>
+    
+
+  )
+}
+
+export default Cta
--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import Banner from "../Banner/Banner";
 import { Outlet } from "react-router";
+import Cta from "../Cta/Cta";
 
 const Layout = () => {
   return (
     <>
       <Banner />
       <Outlet />
-      
+      <Cta/>
     </>
   );
 };
