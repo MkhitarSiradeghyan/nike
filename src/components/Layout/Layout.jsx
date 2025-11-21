606973403e78e9dc3bcd2da2bfaad48f24a5f21b
unknown <miqayelqeshishoxlyan@gmail.com>
2025-11-21 18:21:07 +0400

    feat: order-summary

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Banner from "../Banner/Banner";
 import { Outlet } from "react-router";
 import Hero from "../Hero/Hero";
+import Order from "../Order/Order";
 
 const Layout = () => {
   return (
--- src/components/Order/Order.jsx
+++ src/components/Order/Order.jsx
@@ -4,9 +4,18 @@ import s from "./Order.module.css"
 
 const Order = () => {
   return (
-    <div className="Order">
+    <div className={s.order}>
         <Container>
             <h3>Order Summary</h3>
+            <div className={s.price}>
+                <div className={s.subtotal}>Subtotal</div>
+                <div className={s.discount}>Discount (-20%)</div>
+                <div className={s.delivery}>Delivery Free</div>
+            </div>
+            <div className={s.total}></div>
+            <input className={s.promo} type="text" placeholder='Add promo code'/>
+            <button className={s.apply}>Apply</button>
+            <button className={s.checkout}>Go to Checkout</button>
         </Container>
     </div>
 )
